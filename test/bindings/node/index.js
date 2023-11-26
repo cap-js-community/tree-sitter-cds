9c64e770d4464536a7fb3326318c0dba39c78b3a
Andre Meyering <andre.meyering@sap.com>
2023-11-26 15:41:41 +0100

    style: Introduce ESLint checks for all JavaScript files

--- test/bindings/node/index.js
+++ test/bindings/node/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const Parser = require('tree-sitter');
 const Cds = require('@cap-js-community/tree-sitter-cds');
 
@@ -12,8 +10,14 @@ const tree = parser.parse(sourceCode);
 assertEq(tree.rootNode.child(0).type, 'entity_definition');
 assertEq(tree.rootNode.child(0).nameNode.text, 'Test');
 
-function assertEq(actual, expected) {
-  if (expected !== actual) {
-    throw new Error(`assertion failed: ${actual} === ${expected}`)
+/**
+ * Assert that the lhs is equal to the rhs.
+ *
+ * @param {any} lhs
+ * @param {any} rhs
+ */
+function assertEq(lhs, rhs) {
+  if (lhs !== rhs) {
+    throw new Error(`assertion failed: ${lhs} === ${rhs}`);
   }
 }
